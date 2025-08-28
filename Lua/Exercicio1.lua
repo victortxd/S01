5c6fa96e8564571826f8e311831f5b6d69b762bb
VICTOR HUGO TORRES DIAS <victorghot9@gmail.com>
2025-08-28 08:45:06 -0300

    Create Exercicio1.lua

---
+++ Lua/Exercicio1.lua
@@ -0,0 +1,14 @@
+print("Digite seu primeiro nomero: ")
+local a = tonumber(io.read())
+print("Digite seu segundo numero: ")
+local n = tonumber(io.read())
+
+print("Digite o numero que voce deseja saber o multiplo: ")
+local x = tonumber(io.read())
+
+for i = a, n, 1 do --start, end, step
+  if i % x == 0 then
+   print(i)
+
+  end
+end
