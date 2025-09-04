24001eb23dc1ede36d616dd28676306a6a579004
VICTOR HUGO TORRES DIAS <victorghot9@gmail.com>
2025-09-04 11:04:27 -0300

    Create Exercicio 3.rs

---
+++ Rust/Exercicio 3.rs
@@ -0,0 +1,37 @@
+use std::io;
+
+fn imprimir_tabuada(numero: i32, limite_inferior: i32, limite_superior: i32)
+  {
+    println!("\n=== Tabuada do {} ===", numero);
+    for i in limite_inferior..=limite_superior 
+      {
+        println!("{} x {} = {}", numero, i, numero * i);
+      }
+  }
+
+fn main() 
+  {
+    let mut entrada = String::new();
+
+    println!("Digite o número da tabuada:");
+    io::stdin()
+        .read_line(&mut entrada)
+        .expect("Erro ao ler o número!");
+    let numero: i32 = entrada.trim().parse().expect("Digite um número válido!");
+    entrada.clear();
+
+    println!("Digite o limite inferior:");
+    io::stdin()
+        .read_line(&mut entrada)
+        .expect("Erro ao ler o número!");
+    let limite_inferior: i32 = entrada.trim().parse().expect("Digite um número válido!");
+    entrada.clear();
+
+    println!("Digite o limite superior:");
+    io::stdin()
+        .read_line(&mut entrada)
+        .expect("Erro ao ler o número!");
+    let limite_superior: i32 = entrada.trim().parse().expect("Digite um número válido!");
+
+    imprimir_tabuada(numero, limite_inferior, limite_superior);
+}
