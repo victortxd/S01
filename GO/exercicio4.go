207afec6c4d23e7877136926ffaab4d542fe8c6a
VICTOR HUGO TORRES DIAS <victorghot9@gmail.com>
2025-09-11 23:27:45 -0300

    Update exercicio4.go

--- GO/exercicio4.go
+++ GO/exercicio4.go
@@ -1,1 +1,31 @@
+package main
+import "fmt"
+func ad(e, c string) bool
+{
+	if e == "senha" && c == "admin"
+  {
+		return true
+	} else {
+		return false
+	}
+}
+func main() 
+{
+	var ee string
+	var ec string
 
+	for true
+  {
+		fmt.Print("faça o login: ")
+		fmt.Scanln(&ee)
+		fmt.Print("senha: ")
+		fmt.Scanln(&ec)
+		if ad(ee, ec) == true
+    {
+			fmt.Println("bem vindo(a)!")
+			break
+		} else {
+			fmt.Println("login ou senha inválidas. Tente novamente.")
+		}
+	}
+}
