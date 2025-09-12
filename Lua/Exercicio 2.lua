4c86ab1facff7102711a30ef3a62f00cf1bfbb97
VICTOR HUGO TORRES DIAS <victorghot9@gmail.com>
2025-09-11 21:53:00 -0300

    Update Exercicio 2.lua

--- Lua/Exercicio 2.lua
+++ Lua/Exercicio 2.lua
@@ -6,14 +6,14 @@ print("Digite numeros (digite O para parar): ")
 
 while true do
       valor = tonumber(io.read())
-        if valor == O then --coloquei uma condição de parada para dar um stop na leitura do
+        if valor == O then 
             break
         end
     table.insert(numeros, valor)
     cont = cont + 1
     end
-local maior = numeros[1]--Afirma que o maior numero é o primeiro termo
-for i = 2, cont do --por isso começa a contar a partir do 2
+local maior = numeros[1]
+for i = 2, cont do 
       if numeros[i] > maior then
             maior = numeros[i]
       end
