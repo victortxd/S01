962e7d0e1483c3e899e67c3a232a8e3c42629dad
VICTOR HUGO TORRES DIAS <victorghot9@gmail.com>
2025-09-11 22:15:26 -0300

    Update Exercicio 2.rs

--- Rust/Exercicio 2.rs
+++ Rust/Exercicio 2.rs
@@ -1,50 +1,41 @@
 use std::io;
 
-fn eh_par(numero: i32) -> bool 
+fn eh_par(numero: i32) -> bool
   {
     numero % 2 == 0
-  }
+}
 
 fn main() 
   {
-    let mut entrada = String::new();
+  let mut l1 = String::new();
+  let mut l2 = String::new();
 
-    println!("Jogador 1, escolha 'par' ou 'impar':");
-    io::stdin()
-        .read_line(&mut entrada)
-        .expect("Erro ao ler a entrada.");
-    let escolha_jogador1 = entrada.trim().to_lowercase();
-    entrada.clear();
+  println!("Jogador 1 digite seu número: ");
+  io::stdin()
+    .read_line(&mut l1)
+    .expect("Falha ao ler a entrada");
+  
+  println!("Jogador 2 digite seu número: ");
+  io::stdin()
+    .read_line(&mut l2)
+    .expect("Falha ao ler a entrada");
+  let n1: i32 = l1.trim().parse().expect("Falha ao converter n1 para numero");
+  let n2: i32 = l2.trim().parse().expect("Falha ao converter n2 para numero");
 
-    println!("Jogador 1, digite seu número:");
-    io::stdin()
-        .read_line(&mut entrada)
-        .expect("Erro ao ler o número.");
-    let numero1: i32 = entrada.trim().parse().expect("Digite um número válido!");
-    entrada.clear();
+  let soma = eh_par(n1+n2);
+  let k1 = eh_par(n1);
+  let k2 = eh_par(n2);
 
-    println!("Jogador 2, digite seu número:");
-    io::stdin()
-        .read_line(&mut entrada)
-        .expect("Erro ao ler o número.");
-    let numero2: i32 = entrada.trim().parse().expect("Digite um número válido!");
-
-    let soma = numero1 + numero2;
-
-    let soma_par = eh_par(soma);
-
-    println!("\n=== Resultado do Jogo ===");
-    println!("Jogador 1 escolheu: {}", escolha_jogador1);
-    println!("Número do Jogador 1: {}", numero1);
-    println!("Número do Jogador 2: {}", numero2);
-    println!("Soma dos números: {}", soma);
-    println!("A soma é: {}", if soma_par { "par" } else { "ímpar" });
-
-    if (soma_par && escolha_jogador1 == "par") || (!soma_par && escolha_jogador1 == "impar")
-    {
-        println!(" Jogador 1 venceu!");
-    } else {
-        println!(" Jogador 2 venceu!");
-    }
+  if soma == k1 && soma == k2
+  {
+    println!("Soma: {}", n1+n2);
+    println!("Empate");
+  } else if soma == k1 
+  {
+      println!("Soma: {}", n1+n2);
+      println!("Jogador 1 venceu");
+  } else {
+      println!("Soma: {}", n1+n2);
+      println!("Jogador 2 venceu");
+  }
 }
-
