09cd88a0c0462bedf384cf43c77bae622a72aee9
VICTOR HUGO TORRES DIAS <victorghot9@gmail.com>
2025-09-11 23:01:00 -0300

    Update exercicio2.go

--- GO/exercicio2.go
+++ GO/exercicio2.go
@@ -1,1 +1,16 @@
-a
+package main
+import "fmt"
+func main()
+{
+	fmt.Println("Digite um número: ")
+	var numero int
+	fmt.Scanln(&numero
+             
+	switch numero % 2
+  {
+		case 0:
+			fmt.Println("O número é par")
+		default:
+			fmt.Println("O número é impar")
+	}
+}
