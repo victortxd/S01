34c32c284ea1fc4076cff41c3131dcd5df7a199d
VICTOR HUGO TORRES DIAS <victorghot9@gmail.com>
2025-09-11 23:05:57 -0300

    Update exercicio3.go

--- GO/exercicio3.go
+++ GO/exercicio3.go
@@ -1,1 +1,23 @@
+package main
+import "fmt"
+func fi(n int)
+{
+	var a int = 0
+  var b int = 1
+  
+	for i := 0; i < n; i++ 
+  {
+		fmt.Print(a, " ")
+		a, b = b, a+b
+	}
+}
 
+func main()
+{
+	fmt.Println("Quantos números da sequência de Fibonnaci você quer?: ")
+	
+	var k int
+	fmt.Scanln(&k)
+
+	fi(k)
+}
