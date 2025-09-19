5dbe92511926ae9c2cb85c2a8cbddbb2f6874c09
VICTOR HUGO TORRES DIAS <victorghot9@gmail.com>
2025-09-18 21:21:36 -0300

    Update Exercicio1.cpp

--- C++/Exercicio1.cpp
+++ C++/Exercicio1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+int fib(int ns) 
+{
+    if (ns == 0) 
+    {
+        return 0;
+    }
+    if (ns == 1) 
+    {
+        return 1;
+    }
+        return fib(ns - 1) + fib(ns - 2);
+}
+
+int main()
+{
+      int a;
+  
+      cout << "Digite um numero: ";
+  
+      cin >> a;
+  
+      cout << fib(a) << endl;
+  
+  return 0;
+}
