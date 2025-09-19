8dd00f01d6485580344ceac9b82bd5eec5b3db77
VICTOR HUGO TORRES DIAS <victorghot9@gmail.com>
2025-09-18 21:31:47 -0300

    Update Exercicio2.cpp

--- C++/Exercicio2.cpp
+++ C++/Exercicio2.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+float somarArray(float arr[], int tam)
+{
+	float soma = 0;
+	
+	for(int i = 0; i < tam; i++)
+	{	
+		soma += arr[i];
+	}
+ return soma;
+}
+int main()
+{
+	int tamanho;
+	float arr[100];
+	
+	cout << "Insira a quantidade de numeros: ";
+	  cin >> tamanho;
+	
+	for(int i = 0; i < tamanho; i++)
+	{	
+		  cin >> arr[i];
+	}
+	
+	cout << "Soma: " << somarArray(arr,tamanho);
+	
+	return 0;
