1759ca123fda5e4469ffe0333318afd94ac545f1
VICTOR HUGO TORRES DIAS <victorghot9@gmail.com>
2025-09-18 21:37:36 -0300

    Update Exercicio3.cpp

--- C++/Exercicio3.cpp
+++ C++/Exercicio3.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int n;
+	int soma = 0;
+	float notas;
+	
+	cout << "Quantidade de notas: ";
+	cin >> n;
+	
+	for(int i = 0; i < n; i++)
+	{
+		cin >> notas;
+		
+		while( notas < 0|| notas > 10)
+		{
+			cin >> notas;
+		}
+		
+		soma += notas;
+    
+	}
+  cout << "Sua media e de: " << (soma / n) << endl;
+    if ((soma / n) >= 7) 
+    {
+      cout << "Aprovado!";
+    } else {
+      cout << "Reprovado!";
+    }
+
+  return 0;
+}
